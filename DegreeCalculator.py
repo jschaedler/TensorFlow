407a051b8081df25df0f61c3ba5fa70d0c4bc6e2
jschaedler <jschaedler7@gmail.com>
2021-02-26 18:10:51 -0500

    typos

--- DegreeCalculator.py
+++ DegreeCalculator.py
@@ -1,22 +1,22 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.ERROR)
+# tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38])
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100])
 
 for i, c in enumerate(celsius_q):
-    print("{} degrees celsius = {} degrees fahrenheit".format, fahrenheit_a(i))
+    print("{} degrees celsius = {} degrees fahrenheit".format, fahrenheit_a[i])
 
 l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 model = tf.keras.Sequential([l0])
 
 model.compile(loss='mean_squared_error',
-              optimizer=tf.keras.optimizersAdam(0.1))
+              optimizer=tf.keras.optimizers.Adam(0.1))
 
-history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=false)
+history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
 print('Finished training model')
 
 plt.xlabel('Epoch Number')

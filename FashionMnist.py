986f34007a015e3cb4fc459a8f6ba0671de15dc2
jschaedler <jschaedler7@gmail.com>
2021-03-04 21:05:49 -0500

    first two models

--- FashionMnist.py
+++ FashionMnist.py
@@ -3,14 +3,71 @@ from __future__ import absolute_import, division, print_function
 # TensorFlow and tf.keras
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.ERROR)
+import math
 
-# Helper libraries
+import tensorflow_datasets as tfds
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
 
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
+dataset, metadata = tfds.load(
+    'fashion_mnist', as_supervised=True, with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+class_names = metadata.features['label'].names
+print("Class names: {}".format(class_names))
+
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
 
 
-tf.enable_eager_execution()
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+
+# The map function applies the normalize function to each element in the train
+# and test datasets
+train_dataset = train_dataset.map(normalize)
+test_dataset = test_dataset.map(normalize)
+
+# The first time you use the dataset, the images will be loaded from disk
+# Caching will keep them in memory, making training faster
+train_dataset = train_dataset.cache()
+test_dataset = test_dataset.cache()
+for image, label in test_dataset.take(1):
+    break
+image = image.numpy().reshape((28, 28))
+
+# Plot the image - voila a piece of fashion clothing
+plt.figure()
+plt.imshow(image, cmap=plt.cm.binary)
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+BATCH_SIZE = 32
+train_dataset = train_dataset.cache().repeat().shuffle(
+    num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.cache().batch(BATCH_SIZE)
+
+model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(
+    num_train_examples / BATCH_SIZE))
+
+test_loss, test_accuracy = model.evaluate(
+    test_dataset, steps=math.ceil(num_test_examples/32))
+print('Accuracy on test dataset:', test_accuracy)
